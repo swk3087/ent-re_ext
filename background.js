48fac21bf9b7798a9c2fe5dd8ee4f348222ea5b6
swk <swk1072@gmail.com>
2025-02-15 18:32:37 +0900

    updated

--- background.js
+++ background.js
@@ -1,9 +1,13 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === "complete" && tab.url.includes("playentry.org")) {
+  if (changeInfo.status === "complete" && tab.url && tab.url.includes("playentry.org")) {
     chrome.scripting.executeScript({
       target: { tabId: tabId },
       func: () => {
-        import("https://swk3087.github.io/ent-re/main.js").catch(err => console.error("스크립트 로드 실패:", err));
+        const script = document.createElement("script");
+        script.src = "https://swk3087.github.io/ent-re/main.js";
+        script.type = "text/javascript";
+        script.defer = true;
+        document.head.appendChild(script);
       }
     });
   }
